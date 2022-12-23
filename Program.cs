b6b16bad2053d917456bfe919534be450ac3d065
Sergey <aseev-1977@list.ru>
2022-12-23 17:39:46 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+            int num_A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int num_B = int.Parse(Console.ReadLine());
+
+            if (num_A > num_B)
+            {
+                Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+            }
